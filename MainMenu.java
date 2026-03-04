192d9d149b161cd6db632813fd85a8a1cba214cd
Khush81699 <khushboosgupta864@gmail.com>
2026-03-04 22:05:00 +0530

    Added main menu interface

---
+++ MainMenu.java
@@ -0,0 +1,15 @@
+public class MainMenu {
+
+    public static void menu() {
+
+        System.out.println("\n===============================");
+        System.out.println("  EMPLOYEE MANAGEMENT SYSTEM");
+        System.out.println("===============================");
+        System.out.println("1. Add Employee");
+        System.out.println("2. View Employee");
+        System.out.println("3. Remove Employee");
+        System.out.println("4. Update Employee");
+        System.out.println("5. Exit");
+        System.out.println("===============================");
+    }
+}
